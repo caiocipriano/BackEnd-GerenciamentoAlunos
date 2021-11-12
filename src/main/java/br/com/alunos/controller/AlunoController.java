45b5d684f40bf175033458e6947d53ffd4c55b32
Caio Cipriano <caio.silvax@hotmail.com>
2021-11-12 15:08:40 -0300

    Alteração 12/11 POST

--- src/main/java/br/com/alunos/controller/AlunoController.java
+++ src/main/java/br/com/alunos/controller/AlunoController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +28,11 @@ public class AlunoController {
 	@GetMapping("/aluno/{id}")
 	public Alunos listaAlunos(@PathVariable(value="id") long id) {
 		return alunoRepository.findById(id);
+	}
+	
+	@PostMapping("/aluno")
+	public Alunos salvaAluno(@RequestBody Alunos alunos) {
+		return alunoRepository.save(alunos);
 	}
 	
 }
