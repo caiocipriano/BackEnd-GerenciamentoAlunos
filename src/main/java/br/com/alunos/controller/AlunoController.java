1f2968cf0489af67e3a6d1ede2a9b82dfdf3449f
Caio Cipriano <caio.silvax@hotmail.com>
2021-11-15 14:02:58 -0300

    Alteração 15/11

--- src/main/java/br/com/alunos/controller/AlunoController.java
+++ src/main/java/br/com/alunos/controller/AlunoController.java
@@ -3,9 +3,11 @@ package br.com.alunos.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,5 +36,14 @@ public class AlunoController {
 	public Alunos salvaAluno(@RequestBody Alunos alunos) {
 		return alunoRepository.save(alunos);
 	}
+	
+	@DeleteMapping("/aluno")
+	public void deletaAluno(@RequestBody Alunos alunos) { // void pois não deve me retornar nada
+		alunoRepository.delete(alunos);						//passar ID para delet
+	}
+	
+	
+	
+	
 	
 }
